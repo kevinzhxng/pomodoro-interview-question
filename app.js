c2b079fe74795ce2b3f2c349988a341042e53936
Kevin Zhang <kzhang0826@gmail.com>
2024-08-20 00:07:28 -0400

    done

--- app.js
+++ app.js
@@ -1,37 +1,73 @@
-
-let cancelId
-let startTime
-let timeLeft
-const countdown = 25 * 60 * 1000
-
-const timerMilliseconds = document.querySelector('.timer__milliseconds')
+const timerMilliSeconds = document.querySelector('.timer__milliseconds')
 const timerSeconds = document.querySelector('.timer__seconds')
 const timerMinutes = document.querySelector('.timer__minutes')
+const startButton = document.querySelector('.stopwatch__start')
+const stopButton = document.querySelector('.stopwatch__stop')
+const resetButton = document.querySelector('.stopwatch__reset')
+
+let cancelId;
+let startTime;
+let savedTime = 0;
+const countdown = 25 * 60 * 1000
 
 function startTimer() {
-    startTime = Date.now()
-    cancelId = requestAnimationFrame(updateTimer)
-    
+  startButton.disabled = true
+  stopButton.disabled = false
+  resetButton.disabled = false
+
+  startTime = Date.now();
+  cancelId = requestAnimationFrame(updateTimer)
 }
 
 function stopTimer() {
-    cancelAnimationFrame(cancelId)
-    console.log('stopTimer')
-    startTime = timeLeft;
+  startButton.disabled = false
+  stopButton.disabled = true
+  resetButton.disabled = false
+
+  savedTime += Date.now() - startTime;
+  cancelAnimationFrame(cancelId)
 }
 
 function resetTimer() {
-    console.log('reset')
+  startTime = Date.now();
+  savedTime = 0;
 
+  timerMilliSeconds.innerHTML = "000"
+  timerSeconds.innerHTML = "00";
+  timerMinutes.innerHTML = "25";
 }
 
 function updateTimer() {
-    let millisElapsed = Date.now() - startTime
+  let millisElapsed = Date.now() - startTime + savedTime
+  
+  let millisLeft = countdown - millisElapsed
+  if (millisLeft < 0) {
+    millisLeft = 0;
+    cancelAnimationFrame(cancelId)
+    cancelId = null
+  }
+  let secondsLeft = millisLeft / 1000
+  let minutesLeft = secondsLeft / 60
 
-    timeLeft = countdown - millisElapsed;
+  let millisText = millisLeft % 1000;
+  let secondsText = Math.floor(secondsLeft) % 60;
+  let minutesText = Math.floor(minutesLeft);
 
-    timerMilliseconds.innerHTML = timeLeft % 1000
-    timerSeconds.innerHTML = Math.floor(timeLeft / 1000 % 60)
-    timerMinutes.innerHTML = Math.floor(timeLeft / 1000 / 60 % 60)
+  if (minutesText.toString().length < 2) {
+    minutesText = minutesText.toString().padStart(2, '0')
+  }
+  if (secondsText.toString().length < 2) {
+    secondsText = secondsText.toString().padStart(2, '0')
+  }
+  if (millisText.toString().length < 3) {
+    millisText = millisText.toString().padStart(3, '0')
+  }
+
+  timerMilliSeconds.innerHTML = millisText
+  timerSeconds.innerHTML = secondsText;
+  timerMinutes.innerHTML = minutesText;
+
+  if (cancelId) {
     cancelId = requestAnimationFrame(updateTimer)
+  }
 }
