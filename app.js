7b7f8d007c2fa38543c43f654de5c60f8e7fefeb
Kevin Zhang <kzhang0826@gmail.com>
2024-08-19 23:36:35 -0400

    30% done

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 
+let cancelId
+let startTime
+let timeLeft
+const countdown = 25 * 60 * 1000
+
+const timerMilliseconds = document.querySelector('.timer__milliseconds')
+const timerSeconds = document.querySelector('.timer__seconds')
+const timerMinutes = document.querySelector('.timer__minutes')
 
 function startTimer() {
-    console.log('test')
+    startTime = Date.now()
+    cancelId = requestAnimationFrame(updateTimer)
+    
 }
 
 function stopTimer() {
-    console.log('test')
-
+    cancelAnimationFrame(cancelId)
+    console.log('stopTimer')
+    startTime = timeLeft;
 }
 
 function resetTimer() {
-    console.log('test')
+    console.log('reset')
 
 }
 
 function updateTimer() {
-    console.log('test')
+    let millisElapsed = Date.now() - startTime
+
+    timeLeft = countdown - millisElapsed;
+
+    timerMilliseconds.innerHTML = timeLeft % 1000
+    timerSeconds.innerHTML = Math.floor(timeLeft / 1000 % 60)
+    timerMinutes.innerHTML = Math.floor(timeLeft / 1000 / 60 % 60)
+    cancelId = requestAnimationFrame(updateTimer)
 }
